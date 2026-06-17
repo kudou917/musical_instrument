288ec5d57d1d8576fad4c73436628da7b8a7a361
工藤航大 <kohdaichan@yahoo.co.jp>
2026-06-17 14:02:08 +0900

    センサーテスト、キーパッドピアノの追加

---
+++ sensor_miss/sensortest.c
@@ -0,0 +1,13 @@
+const int SENSOR_PIN = A0;
+
+void setup() {
+    Serial.begin(115200);
+}
+
+void loop() {
+    int value = analogRead(SENSOR_PIN);
+
+    Serial.println(value);
+
+    delay(10);
+}
